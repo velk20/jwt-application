95fbad396a634e5ebbd1fc95d60ddc44d388f2c4
Angel Mladenov <a.mladenov@seeburger.com>
2024-10-28 13:32:58 +0200

    CORS

--- src/main/java/com/jwt/application/config/SecurityConfig.java
+++ src/main/java/com/jwt/application/config/SecurityConfig.java
@@ -17,11 +17,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    private final String[] freeResourceUrls = {
+            "/api-docs/**",
+            "/swagger-ui/**",
+            "/api/v1/auth/authenticate",
+            "/api/v1/auth/register"
+    };
+
+    private final String[] authenticatedResourceUrls = {
+            "/api/v1/**",
+            "/api/v1/user/**",
+            "/api/v1/admin/**"
+    };
 
     private final JwtAuthFilter authFilter;
     private final CustomUserDetailsService customUserDetailsService;
@@ -29,7 +46,8 @@ public class SecurityConfig {
 
     public SecurityConfig(
             JwtAuthFilter authFilter,
-            CustomUserDetailsService customUserDetailsService, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+            CustomUserDetailsService customUserDetailsService,
+            CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
         this.authFilter = authFilter;
         this.customUserDetailsService = customUserDetailsService;
         this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
@@ -37,22 +55,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers(
-                                "/api-docs/**",
-                                "/swagger-ui/**",
-                                "/api/v1/auth/authenticate",
-                                "/api/v1/auth/register").permitAll())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/**").authenticated())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/user/**").authenticated())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/admin/**").authenticated())
+                        auth.requestMatchers(freeResourceUrls).permitAll())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(authenticatedResourceUrls).authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(auth -> auth.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .authenticationProvider(authenticationProvider());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
